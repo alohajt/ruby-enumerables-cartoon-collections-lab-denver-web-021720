f1fe5714caa215840ba81072d919dea014e27c72
JT Lu <alohajt94@gmail.com>
2020-02-05 22:39:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,11 @@
-def roll_call_dwarves(arr)
-  index =0
-  arr.each_with_index {|name, index| puts "#{index+1}. #{name}"
+# def roll_call_dwarves(arr)
+#   index =0
+#   arr.each_with_index {|name, index| puts "#{index+1}. #{name}"
+# end
+
+def roll_call_dwarves(array)
+  index = 0
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
 
 def summon_captain_planet# code an argument here
