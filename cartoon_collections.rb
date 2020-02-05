7665960c3becf6d9c76665724a81b8646114b331
JT Lu <alohajt94@gmail.com>
2020-02-05 23:03:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(arr)
 end
 
 def long_planeteer_calls(arr)
-  arr.filter {|calls| calls.length > 4}
+  arr.any? {|calls| calls.length > 4}
 end
 
 def find_the_cheese# code an argument here
