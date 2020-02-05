59285c5a46b10382961b38a84609b5c991ee69e7
JT Lu <alohajt94@gmail.com>
2020-02-05 23:03:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(arr)
   capArr = []
-  #arr.map {|calls| puts "#{calls.capitalize}!" }
+  #arr.map {|calls| capArr << "#{calls.capitalize}!" }
   arr.map {|calls| calls.capitalize}
   arr.map {|calls| puts "#{calls}!"}
 end
